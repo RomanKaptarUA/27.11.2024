440091cace3140755656fb79adca52a75ad99a22
RomanKaptarUA <romankaptar82@gmail.com>
2024-11-29 17:11:43 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
+// NANOID
+
 // import { nanoid } from 'nanoid';
 // const id = nanoid(5);
 // console.log(id);
 
-import { customAlphabet } from "nanoid";
-const alphabet = 'abc123';
-const custopmNanoid = customAlphabet(alphabet, 10);
+// import { customAlphabet } from "nanoid";
+// const alphabet = 'abc123';
+// const custopmNanoid = customAlphabet(alphabet, 10);
 
-const id = customNAnoid();
-console.log(id);
+// const id = customNAnoid();
+// console.log(id);
+
+////////////////////////////////////////////////////
+
+// PNOTIFY
+
+import { success, notice, info, error, defaultModules } from '@pnotify/core/dist/PNotify.js';
+import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
+import '@pnotify/core/dist/BrightTheme.css';
+
+defaultModules.set(PNotifyMobile, {});
+
+
+// 1
+
+// alert({
+//     text: 'Привіт!'
+// });
+
+// 2
+
+const mySuccess = success({
+    text: 'Успішно'
+});
+const myNotice = notice({
+    text: 'Успішно'
+});
+const  myInfo = info({
+    text: 'Успішно'
+});
+const myError = error({
+    text: 'Успішно'
+});
