3c0af6e0e263e2f5162eceb9bba6c658529e502a
RomanKaptarUA <136634310+RomanKaptarUA@users.noreply.github.com>
2024-11-29 09:14:58 +0000

    Deploying to gh-pages from @ RomanKaptarUA/27.11.2024@586cc89901b85283dc9533ccfc92481bbf8c0824 🚀

---
+++ index.38ad89a7.js
@@ -0,0 +1,2 @@
+let t=t=>crypto.getRandomValues(new Uint8Array(t));((o,e=21)=>{((t,o,e)=>{let l=(2<<Math.log2(t.length-1))-1,n=-~(1.6*l*o/t.length)})(o,0|e,t)})("abc123",10);const o=customNAnoid();console.log(o);
+//# sourceMappingURL=index.38ad89a7.js.map
