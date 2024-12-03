7cdf53d66aaa83c5911fe96d27a1dd50042ca819
RomanKaptarUA <romankaptar82@gmail.com>
2024-12-03 19:13:49 +0200

    ернрне

--- src/index.js
+++ src/index.js
@@ -82,11 +82,87 @@
 
 // BASICLIGHTBOX
 
-import * as basicLightBox from 'basiclightbox';
-import 'basiclightbox/dist/basicLightbox.min.css';
+// import * as basicLightBox from 'basiclightbox';
+// import 'basiclightbox/dist/basicLightbox.min.css';
 
-const image = '<img src="https://i.imgur.com/Zngpr1W.jpeg" alt="foto" width:"500">';
-const show = basicLightBox.create(image);
-document.getElementById('openModal').addEventListener('click', () => {
-    show.show();
+// const image = '<img src="https://i.imgur.com/Zngpr1W.jpeg" alt="foto" width:"500">';
+// const show = basicLightBox.create(image);
+// document.getElementById('openModal').addEventListener('click', () => {
+//     show.show();
+// })
+
+
+
+
+import { success, notice, info, error, defaultModules } from '@pnotify/core/dist/PNotify.js';
+import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
+import '@pnotify/core/dist/BrightTheme.css';
+import { Title } from 'chart.js';
+
+defaultModules.set(PNotifyMobile, {});
+
+
+
+const keys = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+let currentKeyIndex = Math.floor(Math.random() * keys.length);
+let currentKey = keys[currentKeyIndex];
+
+const keyElement = document.getElementById('key');
+keyElement.textContent = `Клавіша: ${currentKey}`;
+
+document.addEventListener('keydown', (event) => {
+   
+    if (event.key.toUpperCase() === currentKey) {
+      
+        currentKeyIndex++;
+
+     
+        if (currentKeyIndex >= keys.length) {
+            currentKeyIndex = 0; 
+        }
+
+
+        currentKey = keys[currentKeyIndex];
+
+      
+        keyElement.textContent = `Клавіша: ${currentKey}`;
+
+
+        let score = parseInt(document.getElementById('score').innerText);
+        score++;
+        document.getElementById('score').innerText = score;
+
+
+        success({
+            title: 'Правильно!',
+            text: 'Ви натиснули правильну клавішу!',
+            delay: 1500
+        });
+
+    } else {
+       
+        error({
+            title: 'Помилка!',
+            text: 'Невірно! Спробуйте ще раз.',
+            delay: 1500
+        });
+    }
+});
+
+document.addEventListener('keypress', () => {
+    event.preventDefault();
 })
+
+document.getElementById('resetButton').addEventListener('click', () => {
+    let score = 0;
+    document.getElementById('score').innerText = score;
+    currentKeyIndex = Math.floor(Math.random() * keys.length);
+    currentKey = keys[currentKeyIndex];
+    keyElement.textContent = `Клавіша: ${currentKey}`; 
+
+    notice({
+        title: 'Гра перезапущена!',
+        text: 'Рахунок скинуто! Починайте знову.',
+        delay: 1500
+    });
+});
