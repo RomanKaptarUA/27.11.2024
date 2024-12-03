06682abb9b82f83e34c1696d85c33ae82ac4644f
RomanKaptarUA <romankaptar82@gmail.com>
2024-12-03 19:29:56 +0200

    .

--- src/index.js
+++ src/index.js
@@ -94,6 +94,20 @@
 
 
 
+
+
+
+
+
+/////////////////////////////////////////
+
+
+
+//1
+
+
+
+
 import { success, notice, info, error, defaultModules } from '@pnotify/core/dist/PNotify.js';
 import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
 import '@pnotify/core/dist/BrightTheme.css';
@@ -166,3 +180,34 @@ document.getElementById('resetButton').addEventListener('click', () => {
         delay: 1500
     });
 });
+
+
+
+
+///////////////////////////////////////////////////////////////////
+
+//2
+import { Chart, registerables } from "chart.js";
+
+Chart.register(...registerables);
+const chartData = {
+    labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"],
+    datasets: [
+      {
+        label: "Продажі за останній місяць",
+        data: [150, 220, 180, 200, 250, 300, 280, 350, 400, 380, 420, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1050, 1100, 1150, 1200, 1250, 1300, 1350],
+        backgroundColor: "#2196f3",
+        borderColor: "#2196f3",
+        borderWidth: 1,
+      },
+    ],
+  };
+  const config = {
+    type: 'line',
+    data: chartData,
+    options: {}
+}
+document.addEventListener('DOMContentLoaded', () => {
+    const a = document.getElementById('sales-chart').getContext('2d');
+    new Chart(a, config);
+})
