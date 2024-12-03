1149ac2e07f47a4472eaa8609ce864f31dc7c659
RomanKaptarUA <romankaptar82@gmail.com>
2024-12-03 18:05:46 +0200

    .

--- src/index.js
+++ src/index.js
@@ -15,11 +15,11 @@
 
 // PNOTIFY
 
-import { success, notice, info, error, defaultModules } from '@pnotify/core/dist/PNotify.js';
-import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
-import '@pnotify/core/dist/BrightTheme.css';
+// import { success, notice, info, error, defaultModules } from '@pnotify/core/dist/PNotify.js';
+// import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
+// import '@pnotify/core/dist/BrightTheme.css';
 
-defaultModules.set(PNotifyMobile, {});
+// defaultModules.set(PNotifyMobile, {});
 
 
 // 1
@@ -30,15 +30,63 @@ defaultModules.set(PNotifyMobile, {});
 
 // 2
 
-const mySuccess = success({
-    text: 'Успішно'
-});
-const myNotice = notice({
-    text: 'Успішно'
-});
-const  myInfo = info({
-    text: 'Успішно'
-});
-const myError = error({
-    text: 'Успішно'
-});
+// const mySuccess = success({
+//     text: 'Успішно'
+// });
+// const myNotice = notice({
+//     text: 'Успішно'
+// });
+// const  myInfo = info({
+//     text: 'Успішно'
+// });
+// const myError = error({
+//     text: 'Успішно'
+// });
+
+
+
+
+
+
+///////////////////////////////////////////////////////
+
+// CHART.JS
+
+// import { Chart, registerables } from "chart.js";
+// import { callback } from "chart.js/helpers";
+
+// Chart.register(...registerables);
+// const date = {
+//     labels: ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august'],
+//     datasets: [{
+//         label: 'temp',
+//         data: [15, 14, 16, 18, 20, 22, 23],
+//         fill: false,
+//         borderColor: 'rgb(80, 192, 190)',
+//         tension: 0.1
+//     }]
+// }
+
+// const config = {
+//     type: 'line',
+//     data: date,
+//     options: {}
+// }
+// document.addEventListener('DOMContentLoaded', () => {
+//     const a = document.getElementById('myChart').getContext('2d');
+//     new Chart(a, config);
+// })
+
+
+///////////////////////////////////////////////////////////////////////////////////
+
+// BASICLIGHTBOX
+
+import * as basicLightBox from 'basiclightbox';
+import 'basiclightbox/dist/basicLightbox.min.css';
+
+const image = '<img src="https://i.imgur.com/Zngpr1W.jpeg" alt="foto" width:"500">';
+const show = basicLightBox.create(image);
+document.getElementById('openModal').addEventListener('click', () => {
+    show.show();
+})
