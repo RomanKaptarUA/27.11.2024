1777ef58de64d2daa6fe6c64f11879dd025fb489
RomanKaptarUA <romankaptar82@gmail.com>
2024-12-11 20:20:10 +0200

    ю

--- src/index.js
+++ src/index.js
@@ -108,78 +108,78 @@
 
 
 
-import { success, notice, info, error, defaultModules } from '@pnotify/core/dist/PNotify.js';
-import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
-import '@pnotify/core/dist/BrightTheme.css';
-import { Title } from 'chart.js';
+// import { success, notice, info, error, defaultModules } from '@pnotify/core/dist/PNotify.js';
+// import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
+// import '@pnotify/core/dist/BrightTheme.css';
+// import { Title } from 'chart.js';
 
-defaultModules.set(PNotifyMobile, {});
+// defaultModules.set(PNotifyMobile, {});
 
 
 
-const keys = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
-let currentKeyIndex = Math.floor(Math.random() * keys.length);
-let currentKey = keys[currentKeyIndex];
+// const keys = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+// let currentKeyIndex = Math.floor(Math.random() * keys.length);
+// let currentKey = keys[currentKeyIndex];
 
-const keyElement = document.getElementById('key');
-keyElement.textContent = `Клавіша: ${currentKey}`;
+// const keyElement = document.getElementById('key');
+// keyElement.textContent = `Клавіша: ${currentKey}`;
 
-document.addEventListener('keydown', (event) => {
+// document.addEventListener('keydown', (event) => {
    
-    if (event.key.toUpperCase() === currentKey) {
+//     if (event.key.toUpperCase() === currentKey) {
       
-        currentKeyIndex++;
+//         currentKeyIndex++;
 
      
-        if (currentKeyIndex >= keys.length) {
-            currentKeyIndex = 0; 
-        }
+//         if (currentKeyIndex >= keys.length) {
+//             currentKeyIndex = 0; 
+//         }
 
 
-        currentKey = keys[currentKeyIndex];
+//         currentKey = keys[currentKeyIndex];
 
       
-        keyElement.textContent = `Клавіша: ${currentKey}`;
+//         keyElement.textContent = `Клавіша: ${currentKey}`;
 
 
-        let score = parseInt(document.getElementById('score').innerText);
-        score++;
-        document.getElementById('score').innerText = score;
+//         let score = parseInt(document.getElementById('score').innerText);
+//         score++;
+//         document.getElementById('score').innerText = score;
 
 
-        success({
-            title: 'Правильно!',
-            text: 'Ви натиснули правильну клавішу!',
-            delay: 1500
-        });
+//         success({
+//             title: 'Правильно!',
+//             text: 'Ви натиснули правильну клавішу!',
+//             delay: 1500
+//         });
 
-    } else {
+//     } else {
        
-        error({
-            title: 'Помилка!',
-            text: 'Невірно! Спробуйте ще раз.',
-            delay: 1500
-        });
-    }
-});
+//         error({
+//             title: 'Помилка!',
+//             text: 'Невірно! Спробуйте ще раз.',
+//             delay: 1500
+//         });
+//     }
+// });
 
-document.addEventListener('keypress', () => {
-    event.preventDefault();
-})
+// document.addEventListener('keypress', () => {
+//     event.preventDefault();
+// })
 
-document.getElementById('resetButton').addEventListener('click', () => {
-    let score = 0;
-    document.getElementById('score').innerText = score;
-    currentKeyIndex = Math.floor(Math.random() * keys.length);
-    currentKey = keys[currentKeyIndex];
-    keyElement.textContent = `Клавіша: ${currentKey}`; 
+// document.getElementById('resetButton').addEventListener('click', () => {
+//     let score = 0;
+//     document.getElementById('score').innerText = score;
+//     currentKeyIndex = Math.floor(Math.random() * keys.length);
+//     currentKey = keys[currentKeyIndex];
+//     keyElement.textContent = `Клавіша: ${currentKey}`; 
 
-    notice({
-        title: 'Гра перезапущена!',
-        text: 'Рахунок скинуто! Починайте знову.',
-        delay: 1500
-    });
-});
+//     notice({
+//         title: 'Гра перезапущена!',
+//         text: 'Рахунок скинуто! Починайте знову.',
+//         delay: 1500
+//     });
+// });
 
 
 
@@ -187,27 +187,27 @@ document.getElementById('resetButton').addEventListener('click', () => {
 ///////////////////////////////////////////////////////////////////
 
 //2
-import { Chart, registerables } from "chart.js";
+// import { Chart, registerables } from "chart.js";
 
-Chart.register(...registerables);
-const chartData = {
-    labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"],
-    datasets: [
-      {
-        label: "Продажі за останній місяць",
-        data: [150, 220, 180, 200, 250, 300, 280, 350, 400, 380, 420, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1050, 1100, 1150, 1200, 1250, 1300, 1350],
-        backgroundColor: "#2196f3",
-        borderColor: "#2196f3",
-        borderWidth: 1,
-      },
-    ],
-  };
-  const config = {
-    type: 'line',
-    data: chartData,
-    options: {}
-}
-document.addEventListener('DOMContentLoaded', () => {
-    const a = document.getElementById('sales-chart').getContext('2d');
-    new Chart(a, config);
-})
+// Chart.register(...registerables);
+// const chartData = {
+//     labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"],
+//     datasets: [
+//       {
+//         label: "Продажі за останній місяць",
+//         data: [150, 220, 180, 200, 250, 300, 280, 350, 400, 380, 420, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1050, 1100, 1150, 1200, 1250, 1300, 1350],
+//         backgroundColor: "#2196f3",
+//         borderColor: "#2196f3",
+//         borderWidth: 1,
+//       },
+//     ],
+//   };
+//   const config = {
+//     type: 'line',
+//     data: chartData,
+//     options: {}
+// }
+// document.addEventListener('DOMContentLoaded', () => {
+//     const a = document.getElementById('sales-chart').getContext('2d');
+//     new Chart(a, config);
+// })
